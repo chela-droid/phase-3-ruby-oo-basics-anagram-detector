b0e65628901c4158e4dd3ee1730d685a6b7e6bea
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-09 10:58:38 +0300

    commit tree ba6c928cc0c577c00f0a2990a31e6abd4bd02e36

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word=word
+    end
+    def initialize(match)
+
+        if "string" =~ /regex/
+            # do something
+          end
+          
+          
+          regex = Regexp.new(/\d{4}-\d{2}-\d{2}\D\d{2}:\d{2}:\d{2}\D/)
+          matchData = regex.match("2015-08-21T13:54:29Z")
+          
+          puts "here" unless matchData
+          
+          
+          
+        # @match!=match
+        # puts ["hello world zombies pants dipper"]
+    end
+end
