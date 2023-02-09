c7628c778cf09fb38b6664b14e7eb96fbe0efa0e
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-09 11:36:15 +0300

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    def initialize(anagram)
+      @anagram = anagram
+    end
+
+    def match(words)
+        words.select do |word|
+          word.chars.sort == @anagram.chars.sort
+        end
+      end
+    end
+    # def match(words)
+        # words.select do |word|
+            # word.chars.sort==@Anagram.chars.sort
+        # end
+    # end
+    
+    # end
+# end
